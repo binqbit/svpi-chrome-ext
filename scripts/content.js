e6840f5237bba03faa85c5458d37f5a877f548af
binqbit <128422584+binqbit@users.noreply.github.com>
2024-12-22 11:47:24 +0200

    add password input checking

--- scripts/content.js
+++ scripts/content.js
@@ -3,6 +3,34 @@ let clickCount = 0;
 let clickTimer = null;
 let selectedInput = null;
 
+function isPasswordInput(element) {
+    if (element.tagName !== 'INPUT') {
+        return false;
+    }
+
+    if (element.type === 'password') {
+        return true;
+    }
+
+    if ('id' in element && element.id.toLowerCase().includes('password')) {
+        return true;
+    }
+
+    if (element.name.toLowerCase().includes('password')) {
+        return true;
+    }
+
+    if (element.className.toLowerCase().includes('password')) {
+        return true;
+    }
+
+    if (element.placeholder.toLowerCase().includes('password')) {
+        return true;
+    }
+
+    return false;
+}
+
 document.addEventListener('click', (event)  =>{
     clickCount ++;
 
@@ -10,7 +38,7 @@ document.addEventListener('click', (event)  =>{
         clickTimer = setTimeout(() => {
             if (clickCount === 2) {
                 const selectedElement = event.target;
-                if (selectedElement.tagName === 'INPUT') {
+                if (isPasswordInput(selectedElement)) {
                     selectedInput = selectedElement;
                     chrome.runtime.sendMessage({ action: "doubleClickInput" });
                 }
